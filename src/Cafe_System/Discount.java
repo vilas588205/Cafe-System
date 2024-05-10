411f2c2facd21361ed700ef156944b0c274b8d5e
vilasdas <your.email@example.com>
2024-05-10 17:48:52 +0530

    First Commit

---
+++ src/Cafe_System/Discount.java
@@ -0,0 +1,15 @@
+package Cafe_System;
+
+public class Discount {
+    double discountThreshold = 50.0;
+    double discountRate = 0.1;
+
+    public double applyDiscount(double allbill) {
+        if (allbill > discountThreshold) {
+            double discountAmount = allbill * discountRate;
+            allbill -= discountAmount;
+            System.out.println("10% discount applied!");
+        }
+        return allbill;
+    }
+}
